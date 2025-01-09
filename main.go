b2bdc318aec645d2cac976277d87088c437b377c
stelzo <stelzo@steado.de>
2025-01-09 22:54:29 +0100

    bump version

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 )
 
 var (
-	DodudaVersion     = "v0.5.6"
+	DodudaVersion     = "v0.5.7"
 	DodudaShort       = "doduda - Unofficial Ankama Launcher CLI"
 	DodudaLong        = "The Ankama Launcher Terminal Client for Developers."
 	DodudaVersionHelp = DodudaShort + "\n" + DodudaVersion + "\nhttps://github.com/dofusdude/doduda"
