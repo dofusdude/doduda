fdba2fa43ab946221e8fb47f16b0bdd0ed85c8f7
stelzo <stelzo@steado.de>
2025-12-06 00:13:48 +0100

    bump version

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 )
 
 var (
-	DodudaVersion     = "v0.6.15"
+	DodudaVersion     = "v0.6.16"
 	DodudaShort       = "doduda - Dofus data CLI"
 	DodudaLong        = "CLI for Dofus asset downloading and unpacking."
 	DodudaVersionHelp = DodudaShort + "\n" + DodudaVersion + "\nhttps://github.com/dofusdude/doduda"
